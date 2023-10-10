e15b5bd9b1557bc366195fd2900d8bb7149836d2
vkaelin <vkaelin@hey.com>
2023-10-10 11:40:09 +0200

    feat: add more integration tests

--- src/test/java/ch/vkaelin/music/api/artist/ArtistControllerTest.java
+++ src/test/java/ch/vkaelin/music/api/artist/ArtistControllerTest.java
@@ -10,36 +10,43 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class ArtistControllerTest {
     @Autowired
-    private MockMvc mockMvc;
+    private ArtistRepository artistRepository;
 
     @Autowired
-    private ArtistRepository artistRepository;
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
 
     private final ObjectMapper mapper = new ObjectMapper()
             .registerModule(new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
 
     @BeforeEach
-    public void init() {
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+        
         UserEntity user = UserEntity.builder()
                 .username("bob_user")
                 .password("1234")
@@ -60,7 +67,7 @@ public class ArtistControllerTest {
 
     @Test
     public void shouldGetArtists() throws Exception {
-        MvcResult result = mockMvc.perform(get("/api/v1/artists"))
+        MvcResult result = mvc.perform(get("/api/v1/artists"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andReturn();
@@ -71,5 +78,35 @@ public class ArtistControllerTest {
         assertNotNull(artists);
         assertEquals(artists.size(), artistRepository.findAll().size());
         assertNotEquals(artists.size(), 0);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    public void shouldDeleteArtist() throws Exception {
+        List<ArtistEntity> artists = artistRepository.findAll();
+        assertEquals(artists.size(), 1);
+
+        Integer artistId = artists.get(0).getId();
+        mvc.perform(delete("/api/v1/artists/" + artistId))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        artists = artistRepository.findAll();
+        assertEquals(artists.size(), 0);
+    }
+
+    @Test
+    @WithMockUser(username = "bob", authorities = {"ARTIST"})
+    public void shouldNotDeleteArtist() throws Exception {
+        List<ArtistEntity> artists = artistRepository.findAll();
+        assertEquals(artists.size(), 1);
+
+        Integer artistId = artists.get(0).getId();
+        mvc.perform(delete("/api/v1/artists/" + artistId))
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+        artists = artistRepository.findAll();
+        assertEquals(artists.size(), 1);
     }
 }
