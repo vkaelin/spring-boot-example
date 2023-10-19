62e212aea5a189372efbd11db3c517d62372c83d
vkaelin <vkaelin@hey.com>
2023-10-19 14:42:08 +0200

    test: add 3 more archtest

--- src/test/java/architecture/NamingConventions.java
+++ src/test/java/architecture/NamingConventions.java
@@ -6,4 +6,6 @@ import lombok.experimental.UtilityClass;
 public class NamingConventions {
     static final String BASE_PACKAGE = "ch.vkaelin.music";
     static final String DOMAIN_PACKAGE = BASE_PACKAGE + ".domain..";
+    static final String API_PACKAGE = BASE_PACKAGE + ".api..";
+    static final String PERSISTENCE_PACKAGE = BASE_PACKAGE + ".persistence..";
 }
